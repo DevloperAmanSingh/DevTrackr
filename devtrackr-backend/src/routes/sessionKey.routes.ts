5a6819a67d2a4d814571765c92cc8c5da41e468b
DevloperAmanSingh <amansinghneh211205@gmail.com>
2025-05-28 22:18:04 +0530

    Integrate validateSessionKeyController into session key routes; add endpoint for validating session keys in sessionKey.routes.ts.

--- devtrackr-backend/src/routes/sessionKey.routes.ts
+++ devtrackr-backend/src/routes/sessionKey.routes.ts
@@ -1,9 +1,9 @@
 import express, { NextFunction } from "express";
-import { generateSessionKeyController } from "../controller/sessionKey.controller";
+import { generateSessionKeyController, validateSessionKeyController } from "../controller/sessionKey.controller";
 import { verifyToken } from "../middlewares/auth.middleware";
 
 const router = express.Router();
 
 router.post("/generate-session-key", verifyToken, generateSessionKeyController);
-
+router.post("/validate-session-key", validateSessionKeyController);
 export default router;
