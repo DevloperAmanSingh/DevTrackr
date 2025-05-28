147e1e4eff822af79d798aea92c88595a6222765
DevloperAmanSingh <amansinghneh211205@gmail.com>
2025-05-28 22:18:20 +0530

    Enhance session key service by updating session key generation logic to include a random identifier and adding a new validateSessionKey function for user validation based on session keys.

--- devtrackr-backend/src/services/sessionKey.service.ts
+++ devtrackr-backend/src/services/sessionKey.service.ts
@@ -1,9 +1,11 @@
-import { PrismaClient } from '@prisma/client';
-import { randomBytes } from 'crypto';
+import { PrismaClient } from "@prisma/client";
+import { randomBytes } from "crypto";
 const prisma = new PrismaClient();
 
 export const generateSessionKey = async (userId: string): Promise<string> => {
-  const sessionKey = randomBytes(24).toString('hex');
+  const randomId = randomBytes(4).toString("hex");
+  const sessionKey = `devtrackr_${randomId}`;
+  console.log("sessionKey", sessionKey);
 
   await prisma.user.update({
     where: { id: userId },
@@ -11,4 +13,16 @@ export const generateSessionKey = async (userId: string): Promise<string> => {
   });
 
   return sessionKey;
+};
+
+export const validateSessionKey = async (sessionKey: string) => {
+  const user = await prisma.user.findFirst({
+    where: { sessionKey },
+  });
+
+  if (!user) {
+    throw new Error('Invalid session key');
+  }
+
+  return user;
 };
